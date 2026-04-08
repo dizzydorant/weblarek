c2a8316956f0b312701ffb2bc20c56f0317018cc
adel shagidullin <shagidullin.adel@mail.ru>
2026-04-09 00:14:59 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/weblarek/',
+   build: {
+    outDir: 'dist',
+  },
   css: {
     preprocessorOptions: {
       scss: {
